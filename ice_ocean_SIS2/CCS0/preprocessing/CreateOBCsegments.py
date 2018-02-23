21745aca45cf6e95a01c8cdfb225f392e34bfe31
Matthew Harrison <Matthew.Harrison@noaa.gov>
2018-02-23 15:30:55 -0500

    preprocessing for CCS1

---
+++ ice_ocean_SIS2/CCS0/preprocessing/CreateOBCsegments.py
@@ -0,0 +1,101 @@
+from brushcutter import lib_obc_variable as lov
+from brushcutter import lib_obc_vectvariable as losv
+from brushcutter import lib_obc_segments as los
+from brushcutter import lib_ioncdf as ncdf
+import subprocess as sp
+import numpy as np
+import netCDF4 as nc
+
+ts_data = 'input_data/global_3d_data.nc'
+uv_data = 'input_data/global_3d_data.nc'
+ssh_data = 'input_data/global_ssh_data.nc'
+momgrd = 'ocean_hgrid.nc'
+srcgrd = 'input_data/ocean_hgrid.nc'
+
+#nt=nc.Dataset(ssh_data).variables['ssh'].shape[0]
+nt=12
+
+
+# ---------- define segments on MOM grid -----------------------
+south = los.obc_segment('segment_001', momgrd,istart=0,iend=50,jstart=0,  jend=0  )
+north = los.obc_segment('segment_002', momgrd,istart=50,iend=0,jstart=42,jend=42)
+west  = los.obc_segment('segment_003', momgrd,istart=0,iend=0,  jstart=42,  jend=0)
+
+# ---------- define variables on each segment ------------------
+temp_south = lov.obc_variable(south,'temp',geometry='surface',obctype='radiation',use_locstream=True)
+temp_north = lov.obc_variable(north,'temp',geometry='surface',obctype='radiation',use_locstream=True)
+temp_west  = lov.obc_variable(west, 'temp',geometry='surface',obctype='radiation',use_locstream=True)
+
+salt_south = lov.obc_variable(south,'salt',geometry='surface',obctype='radiation',use_locstream=True)
+salt_north = lov.obc_variable(north,'salt',geometry='surface',obctype='radiation',use_locstream=True)
+salt_west  = lov.obc_variable(west, 'salt',geometry='surface',obctype='radiation',use_locstream=True)
+
+vel_south = losv.obc_vectvariable(south,'u','v',geometry='surface',obctype='radiation',use_locstream=True)
+vel_north = losv.obc_vectvariable(north,'u','v',geometry='surface',obctype='radiation',use_locstream=True)
+vel_west  = losv.obc_vectvariable(west, 'u','v',geometry='surface',obctype='radiation',use_locstream=True)
+
+zeta_south = lov.obc_variable(south,'zeta',geometry='line',obctype='flather',use_locstream=True)
+zeta_north = lov.obc_variable(north,'zeta',geometry='line',obctype='flather',use_locstream=True)
+zeta_west  = lov.obc_variable(west ,'zeta',geometry='line',obctype='flather',use_locstream=True)
+
+
+xsrc=nc.Dataset(srcgrd).variables['x'][1::2,1::2]
+ysrc=nc.Dataset(srcgrd).variables['y'][1::2,1::2]
+
+
+for kt in np.arange(nt):
+	mm=str(kt+1).zfill(2)
+	# ---------- interpolate T/S from monthly file, frame = kt (jan-dec) and using locstream (x2 speedup)
+ 	temp_south.interpolate_from( ts_data,'thetao',frame=kt,depthname='zt', drown='ncl',\
+	from_global=True,x_coords=xsrc,y_coords=ysrc,method='bilinear')
+	temp_north.interpolate_from( ts_data,'thetao',frame=kt,depthname='zt', drown='ncl', \
+	from_global=True,x_coords=xsrc,y_coords=ysrc,method='bilinear')
+	temp_west.interpolate_from( ts_data,'thetao',frame=kt,depthname='zt', drown='ncl', \
+	from_global=True,x_coords=xsrc,y_coords=ysrc,method='bilinear')
+
+	salt_south.interpolate_from( ts_data,'so',frame=kt,depthname='zt', drown='ncl', \
+	from_global=True,x_coords=xsrc,y_coords=ysrc,method='bilinear')
+	salt_north.interpolate_from( ts_data,'so',frame=kt,depthname='zt', drown='ncl',\
+	from_global=True,x_coords=xsrc,y_coords=ysrc,method='bilinear')
+	salt_west.interpolate_from( ts_data,'so',frame=kt,depthname='zt', drown='ncl', \
+	from_global=True,x_coords=xsrc,y_coords=ysrc,method='bilinear')
+
+	vel_south.interpolate_from( uv_data,'uo','vo',frame=kt,depthname='zt', drown='ncl',  \
+	from_global=True,x_coords_u=xsrc,y_coords_u=ysrc,x_coords_v=xsrc,y_coords_v=ysrc)
+	vel_north.interpolate_from( uv_data,'uo','vo',frame=kt,depthname='zt', drown='ncl', \
+	from_global=True,x_coords_u=xsrc,y_coords_u=ysrc,x_coords_v=xsrc,y_coords_v=ysrc)
+	vel_west.interpolate_from( uv_data,'uo','vo',frame=kt,depthname='zt', drown='ncl', \
+	from_global=True,x_coords_u=xsrc,y_coords_u=ysrc,x_coords_v=xsrc,y_coords_v=ysrc)
+
+	# ---------- set constant value for SSH ----------------------
+	zeta_south.interpolate_from(ssh_data,'ssh',frame=kt,from_global=True,x_coords=xsrc,y_coords=ysrc, drown='ncl')
+	zeta_north.interpolate_from(ssh_data,'ssh',frame=kt,from_global=True,x_coords=xsrc,y_coords=ysrc, drown='ncl')
+	zeta_west.interpolate_from(ssh_data,'ssh',frame=kt,from_global=True,x_coords=xsrc,y_coords=ysrc, drown='ncl')
+
+	# ---------- list segments and variables to be written -------
+	list_segments = [north,south,west]
+#	list_segments = [south]
+
+	list_variables = [temp_south,temp_north,temp_west, \
+	                  salt_south,salt_north,salt_west, \
+	                  zeta_south,zeta_north,zeta_west ]
+#	list_variables = [temp_south,salt_south]
+
+
+        list_vectvariables = [vel_south,vel_north,vel_west]
+#	list_vectvariables = [vel_south]
+
+	#----------- time --------------------------------------------
+	time = temp_south.timesrc
+	time.calendar = nc.Dataset(ts_data).variables['time'].calendar
+
+	# ---------- write to file -----------------------------------
+	ncdf.write_obc_file(list_segments,list_variables,list_vectvariables,time,output='obc_d' + mm + '_CCS0.nc')
+
+# ---------- concat to a single file ---------------------------------
+cmdcat = 'ncrcat obc_d??_CCS0.nc -O -o obc_monthly_CCS0.nc'
+cmdmod = "ncatted -h -a modulo,time,c,c,' ' obc_monthly_CCS0.nc"
+cmdclean = 'rm obc_d??_CCS0.nc'
+sp.call(cmdcat,shell=True)
+sp.call(cmdmod,shell=True)
+sp.call(cmdclean,shell=True)
---
+++ ice_ocean_SIS2/CCS0/preprocessing/IC.py
@@ -0,0 +1,56 @@
+# IPython log file
+#
+#Generate Initial Conditions for CCS1 domain from global
+from midas.rectgrid import *
+import netCDF4 as nc
+import numpy as np
+import matplotlib.pyplot as plt
+
+sgrid=supergrid(file='ocean_hgrid.nc')
+grid=quadmesh(supergrid=sgrid)
+sgrid=supergrid(file='input_data/ocean_hgrid.nc')
+grid_in=quadmesh(supergrid=sgrid,cyclic=True)
+grid.D=nc.Dataset('ocean_topog.nc').variables['depth'][:]
+S=state('input_data/global_3d_data.nc',grid=grid_in,fields=['thetao','so','uo','vo'],time_indices=np.arange(0,1))
+SSH=state('input_data/global_ssh_data.nc',grid=grid_in,fields=['ssh'],time_indices=np.arange(0,1))
+R=S.horiz_interp('thetao',target=grid)
+R=S.horiz_interp('so',target=grid,PrevState=R)
+R=S.horiz_interp('uo',target=grid,PrevState=R)
+R=S.horiz_interp('vo',target=grid,PrevState=R)
+R=SSH.horiz_interp('ssh',target=grid,PrevState=R)
+R.rename_field('thetao','temp')
+R.rename_field('so','salt')
+R.rename_field('uo','u')
+R.rename_field('vo','v')
+R.adjust_thickness('temp')
+R.adjust_thickness('salt')
+R.adjust_thickness('u')
+R.adjust_thickness('v')
+vdict=R.var_dict['temp'].copy()
+vdict['units']='m'
+h=vdict['dz'].copy()
+h=h[np.newaxis,:]
+R.add_field_from_array(h,'h',vdict)
+R.fill_interior('temp')
+#plt.pcolormesh(grid.x_T,grid.y_T,sq(R.temp[0,0,:]));plt.colorbar()
+#plt.show()
+#raise
+R.fill_interior('salt')
+R.fill_interior('u')
+R.fill_interior('v')
+R.fill_interior('ssh')
+R.ssh[:,:,grid.D<0.25]=0.0
+R.v[:,:,np.roll(R.grid.D,shift=-1,axis=0)<0.1]=0.0
+R.v[:,:,0,:]=0.0
+R.v[:,:,-1,:]=0.0
+R.u[:,:,np.roll(R.grid.D,shift=-1,axis=1)<0.1]=0.0
+R.u[:,:,:,0]=0.0
+R.u[:,:,:,-1]=0.0
+
+R.temp=np.ma.filled(R.temp,0.0)
+R.salt=np.ma.filled(R.salt,0.0)
+R.ssh=np.ma.filled(R.ssh,0.0)
+
+R.write_nc('IC.nc',['temp','salt','u','v','h','ssh'],write_interface_positions=True)
+
+
